4ace2a9932c4dedd987008418a701b6a8ea02b7b
Igor Bondarenko <jetmind2@gmail.com>
2016-02-06 22:31:15 +0200

    add humane-test-output to profile.clj

--- profiles.clj
+++ profiles.clj
@@ -1,3 +1,7 @@
-{:user {:plugins [[lein-difftest "2.0.0"]
-                  [lein-cljfmt "0.3.0"]]
-        :signing {:gpg-key "CE27F99B"}}}
+{:user
+ {:dependencies [[pjstadig/humane-test-output "0.7.1"]]
+  :injections [(require 'pjstadig.humane-test-output)
+               (pjstadig.humane-test-output/activate!)]
+  :plugins [[lein-difftest "2.0.0"]
+            [lein-cljfmt "0.3.0"]]
+  :signing {:gpg-key "CE27F99B"}}}
