799ab59dd041558694500fe9eabbec48cbe50035
Igor Bondarenko <jetmind2@gmail.com>
2016-04-22 00:31:39 +0300

    Add clj-refactor deps to profile

--- profiles.clj
+++ profiles.clj
@@ -8,4 +8,7 @@
   :plugins [[lein-difftest "2.0.0"]
             [lein-cljfmt "0.3.0"]]
   :signing {:gpg-key "CE27F99B"}}
-}
+ :repl {:plugins [[cider/cider-nrepl "0.10.0-SNAPSHOT"]
+                  [refactor-nrepl "2.0.0-SNAPSHOT"]]
+        :dependencies [[alembic "0.3.2"]
+                       [org.clojure/tools.nrepl "0.2.12"]]}}
