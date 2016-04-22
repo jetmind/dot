cad9850dcb9020453f5ebb2e3431b4f5777a0af0
Igor Bondarenko <jetmind2@gmail.com>
2016-04-22 21:46:27 +0300

    update deps versions in profiles.clj

--- profiles.clj
+++ profiles.clj
@@ -1,12 +1,12 @@
 {:user
  {:dependencies [
-    [pjstadig/humane-test-output "0.7.1"]
+    [pjstadig/humane-test-output "0.8.0"]
     [org.clojure/tools.namespace "0.2.11"]
   ]
   :injections [(require 'pjstadig.humane-test-output)
                (pjstadig.humane-test-output/activate!)]
   :plugins [[lein-difftest "2.0.0"]
-            [lein-cljfmt "0.3.0"]]
+            [lein-cljfmt "0.5.2"]]
   :signing {:gpg-key "CE27F99B"}}
  :repl {:plugins [[cider/cider-nrepl "0.10.0-SNAPSHOT"]
                   [refactor-nrepl "2.0.0-SNAPSHOT"]]
