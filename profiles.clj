fa05a82da50c26637c24f4ee967576455088c159
Igor Bondarenko <jetmind2@gmail.com>
2016-11-06 23:24:00 +0200

    Update profiles.clj

--- profiles.clj
+++ profiles.clj
@@ -1,14 +1,1 @@
-{:user
- {:dependencies [
-    [pjstadig/humane-test-output "0.8.0"]
-    [org.clojure/tools.namespace "0.2.11"]
-  ]
-  :injections [(require 'pjstadig.humane-test-output)
-               (pjstadig.humane-test-output/activate!)]
-  :plugins [[lein-difftest "2.0.0"]
-            [lein-cljfmt "0.5.2"]]
-  :signing {:gpg-key "CE27F99B"}}
- :repl {:plugins [[cider/cider-nrepl "0.10.0-SNAPSHOT"]
-                  [refactor-nrepl "2.0.0-SNAPSHOT"]]
-        :dependencies [[alembic "0.3.2"]
-                       [org.clojure/tools.nrepl "0.2.12"]]}}
+{:user {:signing {:gpg-key "5FC60CFF"}}}
